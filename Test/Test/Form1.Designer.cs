d06c7d65d4404278323a6cc870e2b89ead2f6a6a
hayka <hayk.aleksanyan@gmail.com>
2020-04-24 14:26:58 +0400

    adding a test project for datagridview_withQuery

---
+++ Test/Test/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.Datagrid_1 = new DataGridView_withQuery.DataGridView_withQuery();
+            this.txtDataRecords = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearchSimple = new System.Windows.Forms.Button();
+            this.btnSearchAdvanced = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagrid_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(23, 14);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(138, 32);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Datagrid_1
+            // 
+            this.Datagrid_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datagrid_1.Location = new System.Drawing.Point(23, 93);
+            this.Datagrid_1.Name = "Datagrid_1";
+            this.Datagrid_1.RowTemplate.Height = 24;
+            this.Datagrid_1.SearchFormTitle = "Search on Test grid";
+            this.Datagrid_1.Size = new System.Drawing.Size(917, 361);
+            this.Datagrid_1.TabIndex = 1;
+            // 
+            // txtDataRecords
+            // 
+            this.txtDataRecords.Location = new System.Drawing.Point(344, 14);
+            this.txtDataRecords.Name = "txtDataRecords";
+            this.txtDataRecords.Size = new System.Drawing.Size(119, 22);
+            this.txtDataRecords.TabIndex = 2;
+            this.txtDataRecords.Text = "500";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of samples";
+            // 
+            // btnSearchSimple
+            // 
+            this.btnSearchSimple.Location = new System.Drawing.Point(488, 14);
+            this.btnSearchSimple.Name = "btnSearchSimple";
+            this.btnSearchSimple.Size = new System.Drawing.Size(126, 32);
+            this.btnSearchSimple.TabIndex = 4;
+            this.btnSearchSimple.Text = "Simple Search";
+            this.btnSearchSimple.UseVisualStyleBackColor = true;
+            this.btnSearchSimple.Click += new System.EventHandler(this.btnSearchSimple_Click);
+            // 
+            // btnSearchAdvanced
+            // 
+            this.btnSearchAdvanced.Location = new System.Drawing.Point(634, 14);
+            this.btnSearchAdvanced.Name = "btnSearchAdvanced";
+            this.btnSearchAdvanced.Size = new System.Drawing.Size(172, 32);
+            this.btnSearchAdvanced.TabIndex = 5;
+            this.btnSearchAdvanced.Text = "Advanced Sarch";
+            this.btnSearchAdvanced.UseVisualStyleBackColor = true;
+            this.btnSearchAdvanced.Click += new System.EventHandler(this.btnSearchAdvanced_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 670);
+            this.Controls.Add(this.btnSearchAdvanced);
+            this.Controls.Add(this.btnSearchSimple);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDataRecords);
+            this.Controls.Add(this.Datagrid_1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Testing datagridview with query";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.Datagrid_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private DataGridView_withQuery.DataGridView_withQuery Datagrid_1;
+        private System.Windows.Forms.TextBox txtDataRecords;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearchSimple;
+        private System.Windows.Forms.Button btnSearchAdvanced;
+    }
+}
+
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public OleDbConnection cnn = new OleDbConnection();
+        public string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C://github//datagridview-with-query//Test//sampleDB.mdb";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cnn.ConnectionString = this.connString;
+            this.cnn.Open();
+
+
+            this.Datagrid_1.AllowUserToAddRows = false;
+            this.Datagrid_1.AllowUserToDeleteRows = false;
+            this.Datagrid_1.EditMode = DataGridViewEditMode.EditProgrammatically;
+            this.Datagrid_1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+
+            this.Datagrid_1.Rows.Clear();
+            this.Datagrid_1.Columns.Clear();
+
+            this.Datagrid_1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing; //fix the header row
+            this.Datagrid_1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing; // fix the leftmost column
+
+            Form1_Resize(sender, e);
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Datagrid_1.Left = btnLoad.Left;
+            this.Datagrid_1.Top = btnLoad.Top + btnLoad.Height + 10;
+            this.Datagrid_1.Width = Width - Datagrid_1.Left - 30;
+            this.Datagrid_1.Height = Height - Datagrid_1.Top - 50;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.cnn != null && this.cnn.State == ConnectionState.Open )
+            {
+                cnn.Close();
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            int RecCount = 0;
+            int u = 0;
+
+            string strSQL = "SELECT Count(Panama.ID) AS CountOfRows FROM Panama where Panama.ID<= " + this.txtDataRecords.Text;
+
+            using (OleDbCommand cmd = new OleDbCommand(strSQL, cnn))
+            {
+                using (OleDbDataReader rdr = cmd.ExecuteReader())
+                {
+                    if (rdr.HasRows)
+                    {
+                        rdr.Read();
+                        if (rdr.IsDBNull(0) == false)
+                        {
+                            RecCount = Convert.ToInt32(rdr.GetValue(0));
+                        }
+                    }
+                }
+            }
+
+
+            strSQL = "SELECT Panama.ID, Panama.address, Panama.fullName, Panama.text_ID, Panama.valid_until, ";
+            strSQL += " Panama.country_code3, Panama.country, Panama.status, Panama.node_ID, Panama.source_ID, Panama.notes, ";
+            strSQL += " Panama.check_status, Panama.test_date,  ";
+            strSQL += "  Panama.test_byte, Panama.test_int, Panama.test_long, Panama.test_single, Panama.test_double, Panama.test_decimal ";
+            strSQL += " FROM Panama ";
+            strSQL += " where Panama.ID<=" + this.txtDataRecords.Text;
+
+            using (OleDbCommand cmd = new OleDbCommand(strSQL, cnn))
+            {
+                using (OleDbDataReader rdr = cmd.ExecuteReader())
+                {
+                    this.Datagrid_1.Rows.Clear();
+                    this.Datagrid_1.Columns.Clear();
+
+                    for (int k = 0; k < rdr.FieldCount; k++)
+                    {
+
+                        switch (rdr.GetFieldType(k).Name)
+                        {
+                            case "Boolean":
+
+                                this.Datagrid_1.Columns.Add(new DataGridViewCheckBoxColumn());
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].CellTemplate = new DataGridViewCheckBoxCell();
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].Name = rdr.GetName(k);
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].ValueType = rdr.GetFieldType(k);
+
+                                break;
+
+                            default:  // default value would be string
+
+                                this.Datagrid_1.Columns.Add(new DataGridViewColumn());
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].CellTemplate = new DataGridViewTextBoxCell();
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].Name = rdr.GetName(k);
+                                this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].ValueType = rdr.GetFieldType(k);
+
+                                break;
+
+                        }
+
+                        this.Datagrid_1.Columns[this.Datagrid_1.Columns.Count - 1].SortMode = DataGridViewColumnSortMode.Automatic;
+
+                    }
+
+                    if (RecCount > 0)
+                    {
+                        this.Datagrid_1.Rows.Add(RecCount);
+
+                        while (rdr.Read())
+                        {
+                            for (int k = 0; k < rdr.FieldCount; k++)
+                            {
+                                this.Datagrid_1.Rows[u].Cells[k].Value = (rdr.IsDBNull(k)) ? "" : rdr.GetValue(k);
+                            }
+                            u++;
+                        }
+                    }
+                }
+            }
+
+
+            MessageBox.Show("Data has been loaded");
+
+        }
+
+        private void btnSearchSimple_Click(object sender, EventArgs e)
+        {
+            this.Datagrid_1.SearchSimpleStart();
+        }
+
+        private void btnSearchAdvanced_Click(object sender, EventArgs e)
+        {
+            this.Datagrid_1.SearchAdvancedStart();
+        }
+    }
+}
